18f85ebdabfcaa47c642944e4722eeb1fa312be5
slleazyhkk <m2408026@edu.misis.ru>
2025-03-12 17:09:48 +0300

    Update Blue_1.cs

--- Blue_1.cs
+++ Blue_1.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Lab_7
 {
@@ -20,7 +21,7 @@ namespace Lab_7
             public int Votes => _votes;
 
             //конструктор
-            public Response(string name, string surname)
+            public Response(string name)
             {
                 _name = name;
                 _votes = 0;
@@ -45,61 +46,61 @@ namespace Lab_7
                 Console.WriteLine(_name + " " + _votes);
             }
 
-            public class HumanResponse : Response
-            { 
-                private string _surname;
 
-                public string Surname => _surname;
 
-                public HumanResponse(string name, string surname) : base(name)
+        }
+        // статический метод сортировки массива ответов Sort(Response[] responses) по значению votes
+        //public static void Sort(Response[] responses)
+        //{
+        //    if (responses==null || responses.Length == 0) return;
+        //    for (int i = 0; i < responses.Length; i++)
+        //    {
+        //        for (int j=0; j < responses.Length - i - 1; j++)
+        //        {
+        //            if (responses[j].Votes < responses[j+1].Votes)
+        //            {
+        //                var temp = responses[j];
+        //                responses[j] = responses[j+1];
+        //                responses[j+1] = temp;
+        //            }
+        //        }
+        //    }
+        //}
+        //вывод
+        public class HumanResponse : Response
+        {
+            private string _surname;
+
+            public string Surname => _surname;
+
+            public HumanResponse(string name, string surname) : base(name)
+            {
+                _surname = surname;
+            }
+
+            public override int CountVotes(Response[] responses)
+            {
+                if (responses == null || responses.Length == 0) return 0;
+
+                foreach (Response response in responses)
                 {
-                    _surname = surname;
-                    _votes = 0;
-                }
-
-                public override int CountVotes(Response[] responses)
-                {
-                    if (responses == null || responses.Length == 0) return 0;
-
-                    foreach (Response response in responses)
+                    if (response != null)
                     {
-                        if (response != null)
+                        HumanResponse human = response as HumanResponse;
+                        if (human != null)
                         {
-                            HumanResponse human = response as HumanResponse;
-                            if (human != null)
-                            {
-                                if (response.Name == this.Name && human.Surname == _surname)
-                                    _votes++;
-                            }
+                            if (response.Name == this.Name && human.Surname == _surname)
+                                _votes++;
                         }
                     }
-                    return _votes;
                 }
-                public override void Print()
-                {
-                    Console.WriteLine(_name + " " +" " +_surname+ _votes);
-                }
-
+                return _votes;
             }
-            // статический метод сортировки массива ответов Sort(Response[] responses) по значению votes
-            //public static void Sort(Response[] responses)
-            //{
-            //    if (responses==null || responses.Length == 0) return;
-            //    for (int i = 0; i < responses.Length; i++)
-            //    {
-            //        for (int j=0; j < responses.Length - i - 1; j++)
-            //        {
-            //            if (responses[j].Votes < responses[j+1].Votes)
-            //            {
-            //                var temp = responses[j];
-            //                responses[j] = responses[j+1];
-            //                responses[j+1] = temp;
-            //            }
-            //        }
-            //    }
-            //}
-            //вывод
-            
+            public override void Print()
+            {
+                Console.WriteLine(Name + " " + Surname + " " + _votes);
+            }
+
         }
     }
 }
