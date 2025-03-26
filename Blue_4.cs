c57a5b99facc4f2fa9a420a163ab96a7e697651d
trixpalina <124019361+trixpalina@users.noreply.github.com>
2025-03-27 01:35:23 +0300

    Update Blue_4.cs

--- Blue_4.cs
+++ Blue_4.cs
@@ -1,178 +1,167 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace Lab_7
+namespace Lab_6
 {
-    public class Blue_4
+    public abstract class Team
     {
-        public abstract class Team
+        protected string _name;
+        protected int[] _scores;
+
+        public string Name => _name;
+        public int[] Scores
         {
-            protected string _name;
-            protected int[] _scores;
-
-            public string Name => _name;
-            public int TotalScore
+            get
             {
-                get
-                {
-                    int total = 0;
-                    foreach (var score in _scores)
-                        total += score;
-                    return total;
-                }
-            }
-
-            public Team(string name)
-            {
-                _name = name;
-                _scores = new int[0];
-            }
-
-            public void PlayMatch(int result)
-            {
-                int[] newarr = new int[_scores.Length + 1];
+                if (_scores == null) return null;
+                int[] newarr = new int[_scores.Length];
                 for (int i = 0; i < _scores.Length; i++)
                     newarr[i] = _scores[i];
-                newarr[_scores.Length] = result;
-                _scores = newarr;
+                return newarr;
             }
-
-            public void Print()
+        }
+        public int TotalScore
+        {
+            get
             {
-                Console.WriteLine($"{_name}: {TotalScore}");
+                if (_scores == null || _scores.Length == 0) return 0;
+                int total = 0;
+                for (int i = 0; i < _scores.Length; i++)
+                    total += _scores[i];
+                return total;
             }
         }
 
-        public class ManTeam : Team
+        protected Team(string name)
         {
-            public ManTeam(string name) : base(name) { }
+            _name = name;
+            _scores = new int[0];
         }
 
-        public class WomanTeam : Team
+        public void PlayMatch(int result)
         {
-            public WomanTeam(string name) : base(name) { }
+            int[] newarr = new int[_scores.Length + 1];
+            for (int i = 0; i < _scores.Length; i++)
+                newarr[i] = _scores[i];
+            newarr[_scores.Length] = result;
+            _scores = newarr;
         }
 
-        public class Group
+        public void Print()
         {
-            private string _name;
-            private ManTeam[] _manTeams;
-            private WomanTeam[] _womanTeams;
-            private int _manCount;
-            private int _womanCount;
+            Console.Write($"{_name}: {TotalScore}");
+            Console.WriteLine("");
+        }
+    }
 
-            public string Name => _name;
-            public ManTeam[] ManTeams => _manTeams;
-            public WomanTeam[] WomanTeams => _womanTeams;
+    public class ManTeam : Team
+    {
+        public ManTeam(string name) : base(name) { }
+    }
 
-            public Group(string name)
+    public class WomanTeam : Team
+    {
+        public WomanTeam(string name) : base(name) { }
+    }
+
+    public class Group
+    {
+        private string _name;
+        private Team[] _manTeams;
+        private Team[] _womanTeams;
+        private int _manCount;
+        private int _womanCount;
+
+        public string Name => _name;
+        public Team[] ManTeams => _manTeams;
+        public Team[] WomanTeams => _womanTeams;
+
+        public Group(string name)
+        {
+            _name = name;
+            _manTeams = new Team[12];
+            _womanTeams = new Team[12];
+            _manCount = 0;
+            _womanCount = 0;
+        }
+
+        public void Add(Team team)
+        {
+            if (team is ManTeam && _manCount < 12)
+                _manTeams[_manCount++] = team;
+            else if (team is WomanTeam && _womanCount < 12)
+                _womanTeams[_womanCount++] = team;
+        }
+
+        public void Add(Team[] teams)
+        {
+            for (int i = 0; i < teams.Length; i++)
+                Add(teams[i]);
+        }
+
+        private void SortTeams(ref Team[] teams, int count)
+        {
+            for (int i = 0; i < count - 1; i++)
             {
-                _name = name;
-                _manTeams = new ManTeam[12];
-                _womanTeams = new WomanTeam[12];
-                _manCount = 0;
-                _womanCount = 0;
-            }
-
-            public void Add(Team team)
-            {
-                if (team is ManTeam && _manCount < 12)
+                for (int j = 0; j < count - i - 1; j++)
                 {
-                    _manTeams[_manCount++] = (ManTeam)team;
-                }
-                else if (team is WomanTeam && _womanCount < 12)
-                {
-                    _womanTeams[_womanCount++] = (WomanTeam)team;
-                }
-            }
-
-            public void Add(Team[] teams)
-            {
-                for (int i = 0; i < teams.Length; i++)
-                {
-                    Add(teams[i]);
-                }
-            }
-
-            public void Sort()
-            {
-                if (_manTeams == null || _manCount < 2) return;
-                for (int i = 0; i < _manCount - 1; i++)
-                {
-                    for (int j = 0; j < _manCount - i - 1; j++)
+                    if (teams[j].TotalScore < teams[j + 1].TotalScore)
                     {
-                        if (_manTeams[j].TotalScore < _manTeams[j + 1].TotalScore)
-                        {
-                            ManTeam temp = _manTeams[j];
-                            _manTeams[j] = _manTeams[j + 1];
-                            _manTeams[j + 1] = temp;
-                        }
+                        Team temp = teams[j];
+                        teams[j] = teams[j + 1];
+                        teams[j + 1] = temp;
                     }
                 }
-
-                if (_womanTeams == null || _womanCount < 2) return;
-                for (int i = 0; i < _womanCount - 1; i++)
-                {
-                    for (int j = 0; j < _womanCount - i - 1; j++)
-                    {
-                        if (_womanTeams[j].TotalScore < _womanTeams[j + 1].TotalScore)
-                        {
-                            WomanTeam temp = _womanTeams[j];
-                            _womanTeams[j] = _womanTeams[j + 1];
-                            _womanTeams[j + 1] = temp;
-                        }
-                    }
-                }
-            }
-
-            public static Group Merge(Group group1, Group group2, int size)
-            {
-                Group result = new Group("Финалисты");
-                group1.Sort();
-                group2.Sort();
-
-                int i = 0, j = 0;
-                while (i < size / 2 && j < size / 2)
-                {
-                    if (group1.ManTeams[i].TotalScore >= group2.ManTeams[j].TotalScore)
-                        result.Add(group1.ManTeams[i++]);
-                    else
-                        result.Add(group2.ManTeams[j++]);
-                }
-                while (i < size / 2) result.Add(group1.ManTeams[i++]);
-                while (j < size / 2) result.Add(group2.ManTeams[j++]);
-
-                i = 0;
-                j = 0;
-                while (i < size / 2 && j < size / 2)
-                {
-                    if (group1.WomanTeams[i].TotalScore >= group2.WomanTeams[j].TotalScore)
-                        result.Add(group1.WomanTeams[i++]);
-                    else
-                        result.Add(group2.WomanTeams[j++]);
-                }
-                while (i < size / 2) result.Add(group1.WomanTeams[i++]);
-                while (j < size / 2) result.Add(group2.WomanTeams[j++]);
-
-                return result;
-            }
-
-            public void Print()
-            {
-                Console.WriteLine(Name);
-                Print(_manTeams);
-                Print(_womanTeams);
-            }
-            private void Print(Team[] team)
-            {
-                if (team == null) return;
-                foreach (var i in team) Console.Write(i + " ");
-                Console.WriteLine();
             }
         }
 
+        public void Sort()
+        {
+            SortTeams(ref _manTeams, _manCount);
+            SortTeams(ref _womanTeams, _womanCount);
+        }
+
+        public static Group Merge(Group group1, Group group2)
+        {
+            Group result = new Group("Финалисты");
+            group1.Sort();
+            group2.Sort();
+
+            int i = 0, j = 0;
+            while (i < 6 && j < 6)
+            {
+                if (group1.ManTeams[i].TotalScore >= group2.ManTeams[j].TotalScore)
+                    result.Add(group1.ManTeams[i++]);
+                else
+                    result.Add(group2.ManTeams[j++]);
+            }
+            while (i < 6) result.Add(group1.ManTeams[i++]);
+            while (j < 6) result.Add(group2.ManTeams[j++]);
+
+            i = 0; j = 0;
+            while (i < 6 && j < 6)
+            {
+                if (group1.WomanTeams[i].TotalScore >= group2.WomanTeams[j].TotalScore)
+                    result.Add(group1.WomanTeams[i++]);
+                else
+                    result.Add(group2.WomanTeams[j++]);
+            }
+            while (i < 6) result.Add(group1.WomanTeams[i++]);
+            while (j < 6) result.Add(group2.WomanTeams[j++]);
+
+            return result;
+        }
+
+        public void Print()
+        {
+            Console.Write($"{_name}:\nМужские команды:\n");
+            for (int i = 0; i < _manCount; i++)
+                _manTeams[i].Print();
+
+            Console.Write("Женские команды:\n");
+            for (int i = 0; i < _womanCount; i++)
+                _womanTeams[i].Print();
+
+            Console.WriteLine("");
+        }
     }
 }
