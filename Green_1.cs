e08a0be98c12fbc7899a96213c9528e41e7a7a1f
Petr2101 <petrkaravaev011@gmail.com>
2025-03-28 02:58:29 +0300

    Add files via upload

---
+++ Green_1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab_7
+{
+    public class Green_1
+    {
+        public abstract class Participant
+        {
+            private string _surname;
+            private string _group;
+            private string _trainer;
+            private double _result;
+            private bool _hasResult;
+            private protected double _standard;
+            private static int _countPassed;
+            private static bool _isPrinted = false;
+            private static bool _isPrintedTrainer = false;
+            static Participant()
+            {
+                _countPassed = 0;
+            }
+            public static Participant[] FindParticipantsByTrainer(Participant[] list, Type type, string trainerName)
+            {
+                int num = 0;
+                foreach (var p in list)
+                {
+                    if (p != null && p.GetType() == type && p.Trainer == trainerName)
+                    {
+                        num++;
+                    }
+                }
+                Participant[] filtered = new Participant[num];
+                int index = 0;
+                foreach (var p in list)
+                {
+                    if (p != null && p.GetType() == type && p.Trainer == trainerName)
+                    {
+                        filtered[index++] = p;
+                    }
+                }
+                return filtered;
+            }
+            public Participant(string surname, string group, string trainer)
+            {
+                _standard = 100;
+                _surname = surname;
+                _group = group;
+                _trainer = trainer;
+                _result = 0;
+                _hasResult = false;
+            }
+            public string Surname => _surname;
+            public string Group => _group;
+            public string Trainer => _trainer;
+            public double Result => _result;
+            public bool HasPassed => _hasResult && _result <= _standard && _result > 0;
+            public static int PassedCount => _countPassed;
+            public void Run(double time)
+            {
+                if (time <= 0 || _hasResult)
+                {
+                    return;
+                }
+                _result = time;
+                _hasResult = true;
+
+                if (time <= _standard)
+                {
+                    _countPassed++;
+                }
+            }
+            public void Print()
+            {
+                if (!_isPrinted)
+                {
+                    Console.WriteLine("Число участников, выполнивших норматив: {0}", PassedCount);
+                    _isPrinted = true;
+                }
+                Console.WriteLine("{0,-12} {1,-10} {2,-12} {3,-10:F2} {4,-10}", Surname, Group, Trainer, Result, HasPassed);
+            }
+        }
+        public class Sprinter100M : Participant
+        {
+            public Sprinter100M(string surname, string group, string trainer) : base(surname, group, trainer)
+            {
+                _standard = 12;
+            }
+        }
+        public class Sprinter500M : Participant
+        {
+            public Sprinter500M(string surname, string group, string trainer) : base(surname, group, trainer)
+            {
+                _standard = 90;
+            }
+        }
+    }
+}
---
+++ Green_3.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab_7
+{
+    public class Green_3
+    {
+        public class Student .
+        {
+            private string _name; 
+            private string _surname; 
+            private int[] _marks;
+            private bool _isExpelled;
+            private int _examCount; 
+            private int _id; 
+            private static int _nextId; 
+            private bool _restored;
+            static Student() 
+            {
+                _nextId = 1;
+            }
+            public Student(string name, string surname)
+            {
+                _id = _nextId++;
+                _name = name;
+                _surname = surname;
+                _marks = new int[3];
+                for (int i = 0; i < _marks.Length; i++)
+                {
+                    _marks[i] = 0;
+                }
+                _isExpelled = false;
+                _examCount = 0;
+                _restored = false;
+            }
+            public string Name => _name; 
+            public string Surname => _surname; 
+            public int[] Marks => (int[])_marks.Clone(); 
+            public int ID => _id; 
+            public double AvgMark 
+            {
+                get
+                {
+                    if (_marks == null || _examCount == 0)
+                    {
+                        return 0;
+                    }
+                    double sum = 0;
+                    for (int i = 0; i < _examCount; i++)
+                    {
+                        sum += _marks[i];
+                    }
+                    return sum / _examCount;
+                }
+            }
+            public bool IsExpelled 
+            {
+                get
+                {
+                    if (_restored) return false;
+                    if (_examCount == 0) return false;
+                    return _marks.Take(_examCount).Any(m => m < 3);
+                }
+            }
+            public void Exam(int mark) 
+            {
+                if (_examCount >= 3 || (_isExpelled && !_restored))
+                {
+                    return;
+                }
+                if (_restored)
+                {
+                    _restored = false;
+                    _isExpelled = false;
+                }
+                _marks[_examCount++] = mark;
+
+                if (mark < 3)
+                {
+                    _isExpelled = true;
+                }
+            }
+            public void Restore() 
+            {
+                _restored = true;
+                _isExpelled = false;
+            }
+            public static void SortByAvgMark(Student[] array) 
+            {
+                if (array == null || array.Length == 0) return;
+
+                bool swapped;
+                do
+                {
+                    swapped = false;
+                    for (int i = 0; i < array.Length - 1; i++)
+                    {
+                        if (array[i].AvgMark < array[i + 1].AvgMark)
+                        {
+                            (array[i], array[i + 1]) = (array[i + 1], array[i]);
+                            swapped = true;
+                        }
+                    }
+                } while (swapped);
+            }
+            public void Print()
+            {
+                Console.WriteLine("{0,-12} {1,-10} {2,-15:F2} {3,-10}", Name, Surname, AvgMark, IsExpelled);
+            }
+        }
+        public class Commission 
+        {
+            public static void Sort(Student[] students)
+            {
+                bool swapped;
+                do
+                {
+                    swapped = false;
+                    for (int i = 0; i < students.Length - 1; i++)
+                    {
+                        if (students[i].ID > students[i + 1].ID)
+                        {
+                            (students[i], students[i + 1]) = (students[i + 1], students[i]);
+                            swapped = true;
+                        }
+                    }
+                } while (swapped);
+            }
+            public static Student[] Expel(ref Student[] students) 
+            {
+                Student[] expelled = students.Where(s => s.IsExpelled).ToArray();
+                students = students.Where(s => !s.IsExpelled).ToArray();
+                return expelled;
+            }
+            public static void Restore(ref Student[] students, Student restored) 
+            {
+                if (students.Any(s => s.ID == restored.ID)) return;
+                if (restored.IsExpelled && restored.ID >= 1)
+                {
+                    int insertPos = Math.Min(restored.ID - 1, students.Length);
+                    students = students.Take(insertPos)
+                                       .Append(restored)
+                                       .Concat(students.Skip(insertPos))
+                                       .ToArray();
+                }
+            }
+        }
+    }
+}
